3b5851be4d27a16f32020371ecbee20ec74b8335
vishal karhade <sarita34karhade@gmail.com>
2021-12-10 20:19:04 +0530

    code changes

--- src/main/java/com/example/controller/UrlShortnerController.java
+++ src/main/java/com/example/controller/UrlShortnerController.java
@@ -1,8 +1,30 @@
 package com.example.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.model.Url;
 
 @RestController
 public class UrlShortnerController {
 
+	@PostMapping("/getShortUrl")
+	public ResponseEntity<Url> getShortUrl(@RequestBody Url url) {
+		return null;
+
+	}
+
+	@GetMapping("/getFullUrl")
+	public ResponseEntity<Url> getFullUrl(@RequestBody Url url) {
+		return null;
+
+	}
+
+	@GetMapping("/redirectUrl")
+	public void redirectUrl() {
+
+	}
 }
