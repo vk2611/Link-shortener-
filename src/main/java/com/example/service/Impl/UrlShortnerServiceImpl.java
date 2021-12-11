e8da3c76648837bc9d9184f79312307d3359c847
vishal karhade <sarita34karhade@gmail.com>
2021-12-11 11:26:58 +0530

    redis enable and disable code added

--- src/main/java/com/example/service/Impl/UrlShortnerServiceImpl.java
+++ src/main/java/com/example/service/Impl/UrlShortnerServiceImpl.java
@@ -4,11 +4,13 @@ import java.nio.charset.Charset;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import com.example.model.Url;
 import com.example.repository.UrlShortnerRepository;
 import com.example.service.UrlShortnerService;
+import com.example.util.UrlRedisCacheUtil;
 import com.google.common.hash.Hashing;
 
 import lombok.extern.slf4j.Slf4j;
@@ -20,6 +22,12 @@ public class UrlShortnerServiceImpl implements UrlShortnerService {
 	@Autowired
 	private UrlShortnerRepository urlShortnerRepository;
 
+	@Autowired
+	private UrlRedisCacheUtil urlRedisCacheUtil;
+
+	@Autowired
+	Environment enviroment;
+
 	@Override
 	public Url createShortUrl(Url url) {
 		String shortUrl = Hashing.murmur3_32().hashString(url.getFullUrl(), Charset.defaultCharset()).toString();
@@ -30,7 +38,13 @@ public class UrlShortnerServiceImpl implements UrlShortnerService {
 
 	@Override
 	public Url getUrl(String shortUrl) {
-		Url url = urlShortnerRepository.findByShortUrl(shortUrl);
+		String redisCheck = enviroment.getProperty("redisCheck");
+		Url url = null;
+		if (Boolean.parseBoolean(redisCheck)) {
+			url = urlRedisCacheUtil.findByShortUrl(shortUrl);
+		} else {
+			url = urlShortnerRepository.findByShortUrl(shortUrl);
+		}
 		return url;
 	}
 
---
+++ src/main/java/com/example/util/UrlRedisCacheUtil.java
@@ -0,0 +1,15 @@
+package com.example.util;
+
+import org.springframework.stereotype.Component;
+
+import com.example.model.Url;
+
+@Component
+public class UrlRedisCacheUtil {
+
+	public Url findByShortUrl(String shortUrl) {
+
+		return null;
+	}
+
+}
